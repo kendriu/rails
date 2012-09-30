a898cdb558d1207a03dc273d96719b5b3be66044
Andrzej Skupień <kendriu@gmail.com>
2012-09-30 19:37:49 +0200

    Task E: A Smarter Cart

--- depot/app/models/line_item.rb
+++ depot/app/models/line_item.rb
@@ -1,5 +1,10 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id, :product, :cart
+  attr_accessible :cart_id, :product_id 
   belongs_to :product
   belongs_to :cart
+
+  def total_price
+    product.price * quantity
+  end
+
 end
