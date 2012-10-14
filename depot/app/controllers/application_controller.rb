0347e843e2f6faf2111b8db1cd12830f600fb4f7
Andrzej Skupień <kendriu@gmail.com>
2012-10-14 20:02:23 +0200

    Repaired cart cleaning

--- depot/app/controllers/application_controller.rb
+++ depot/app/controllers/application_controller.rb
@@ -13,9 +13,9 @@ class ApplicationController < ActionController::Base
 
   def destroy_cart
     if session[:cart_id]
-      cart = current_cart
+      @cart = current_cart
       session[:cart_id] = nil
-      cart.destroy
+      @cart.destroy
     end
   end
 end
